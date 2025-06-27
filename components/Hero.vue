a9c206538114988467cdd8a138c762a913a24e98
towerhufham <32599109+towerhufham@users.noreply.github.com>
2025-06-27 16:06:46 -0400

    Image sizing & Hero bg

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="md:hero bg-base-200 md:min-h-screen">
+  <section class="md:hero bg-base-200 md:min-h-screen" id="hero">
     <div class="hero-content text-center">
       <div>
         <h1 class="text-4xl md:text-5xl font-bold">Need some <span class="text-vue">Vue?</span></h1>
         <p class="py-6 text-3xl">
           I got you.
         </p>
-        <div class="stats shadow max-md:stats-vertical">
+        <div class="stats bg-white shadow-lg border border-solid border-slate-300 max-md:stats-vertical">
           <Stat 
             iconName="flowbite:vue-solid" 
             color="text-vue" 
@@ -33,3 +33,12 @@
     </div>
   </section>
 </template>
+
+<style scoped>
+  #hero {
+    background-size: 40px 40px;
+    background-image:
+      linear-gradient(to right, rgb(216, 216, 216) 1px, transparent 1px),
+      linear-gradient(to bottom, rgb(216, 216, 216) 1px, transparent 1px);
+  }
+</style>
